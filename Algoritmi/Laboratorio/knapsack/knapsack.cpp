54e2b77c86d8c60bd7be5749f4767191bc1e0f3c
atipiJ <atipiJ@users.noreply.github.com>
2018-03-04 00:03:47 +0100

    aggiornata knapsack da ricorsiva a iterativa (ancora non funzionante)

--- Algoritmi/Laboratorio/knapsack/knapsack.cpp
+++ Algoritmi/Laboratorio/knapsack/knapsack.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-int knapsack(int C, vector<int> &P, vector<int> &V, int i);
+int knapsack(int C, vector<int> &P, vector<int> &V, int N, vector< vector<int> > &D);
 
 int main(int argc, char **argv){
 
@@ -22,6 +22,15 @@ int main(int argc, char **argv){
   vector<int> P;
   vector<int> V;
 
+  vector< vector<int> > D;
+  D.resize(N+1);
+  for (int i = 0; i < N+1; i++) {
+    D[i].resize(C+1);
+    D[i][0] = 0;
+  }
+  for (int j = 0; j < C+1; j++) {
+    D[0][j] = 0;
+  }
   P.resize(N);
   V.resize(N);
 
@@ -30,20 +39,24 @@ int main(int argc, char **argv){
     input >> V[i];
   }
 
-  int sol = knapsack(C, P, V, N-1);
+  int sol = knapsack(C, P, V, N-1, D);
   cout << sol << endl;
   output << sol;
   return 0;
 }
 
-int knapsack(int C, vector<int> &P, vector<int> &V, int i){
-  if (i > 0){
-    int a = V[i] + knapsack(C-P[i], P, V, i-1);
-    int b = knapsack(C, P, V, i-1);
-    return max(a,b);
-  } else if(C<0){
-    return INT_MIN;
-  } else {
-    return 0;
+int knapsack(int C, vector<int> &P, vector<int> &V, int N, vector< vector<int> > &D){
+  int currentMax = 0;
+  for (int i = 1; i < N; i++) {
+    for (int j = 1; j < C; j++) {
+      int nottaken = D[i-1][j];
+      int taken = INT_MIN;
+      if(j - P[i-1] >= 0){
+        taken = V[i-1] + D[i-1][j - P[i-1]];
+      }
+      D[i][j] = std::max(taken, nottaken);
+      currentMax = std::max(currentMax, D[i][j]);
+    }
   }
+  return currentMax;
 }
